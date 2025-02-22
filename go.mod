f869ce3d4e621d6c419b6b0827c58ce2a47b34a7
wk <wojciechkoko@gmail.com>
2025-02-22 17:07:14 +0100

    Init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ember_lang
+
+go 1.24.0
---
+++ lexer/lexer.go
@@ -0,0 +1,68 @@
+package lexer
+
+import (
+	"ember_lang/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // current pos in input
+	readPosition int  // current reading position
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	// Check if end of input
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	// Update position
+	l.position = l.readPosition
+
+	// Move to next character
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	// Move to next character
+	l.readChar()
+
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
