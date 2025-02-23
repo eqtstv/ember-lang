cfd9e85281021666b11e494682eb5fa99e314a0a
wk <wojciechkoko@gmail.com>
2025-02-23 11:08:52 +0100

    Objects

---
+++ ember_lang/object/object.go
@@ -0,0 +1,69 @@
+package object
+
+import "fmt"
+
+// ----------------------------------------------------------------------------
+// Object Types
+// ----------------------------------------------------------------------------
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ ObjectType = "INTEGER"
+	BOOLEAN_OBJ ObjectType = "BOOLEAN"
+	NULL_OBJ    ObjectType = "NULL"
+)
+
+// ----------------------------------------------------------------------------
+// Object Interface
+// ----------------------------------------------------------------------------
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// ----------------------------------------------------------------------------
+// Integer Object
+// ----------------------------------------------------------------------------
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+// ----------------------------------------------------------------------------
+// Boolean Object
+// ----------------------------------------------------------------------------
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+// ----------------------------------------------------------------------------
+// Null Object
+// ----------------------------------------------------------------------------
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
